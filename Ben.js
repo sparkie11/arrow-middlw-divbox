a1622c12a797bdadae2e61720df574cc436fd07c
sparkcodes <122539340+sparkie11@users.noreply.github.com>
2023-05-01 12:53:06 +0530

    Add files via upload

---
+++ Ben.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Ben.css";
+
+const Ben = () => {
+  return (
+    <div>
+      <div className="yahoo">
+        <h1>“I am vengeance! I am the night! I am Batman!”</h1>
+        <div className="google">
+          {" "}
+          <h2>
+            You either die a hero or u live long enough to become a villain
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ben;
